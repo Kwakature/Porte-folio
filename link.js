125c700d38fc5d51f84e5c854f6005fa7c66098c
kwakature <berton.arthur24@gmail.com>
2025-10-24 08:58:23 +0200

    fix:link

--- link.js
+++ link.js
@@ -1,12 +1,12 @@
-document.getElementById('contact_mail').addEventListener('click', function() {
+document.querySelector('#contact_mail').addEventListener('click', function() {
     window.location.href = "contact.html";
 });
-document.getElementById('linke').addEventListener('click', function() {
+document.querySelector('#linke').addEventListener('click', function() {
     window.location.href = "https://linkedin.com/in/arthur-berton-6b1200338";
 });
-document.getElementById('git').addEventListener('click', function() {
+document.querySelector('#git').addEventListener('click', function() {
     window.location.href = "https://github.com/Kwakature";
 });
-document.getElementById('projet_eff').addEventListener('click', function() {
+document.querySelector('#projet_eff').addEventListener('click', function() {
     window.location.href = "https://github.com/Kwakature/fil-rouge-EFFICOM";
 });
